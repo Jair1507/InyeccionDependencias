8ac01091b99ef808e8fb475330eea611598c1f5e
Jair1507 <85075750+Jair1507@users.noreply.github.com>
2021-11-08 17:51:08 -0600

    Initial commit

---
+++ src/main/java/mx/edu/uacm/Application.java
@@ -0,0 +1,26 @@
+package mx.edu.uacm;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+	
+	private static final Logger log = LogManager.getLogger(Application.class);
+	public static void main(String[] args) {
+		
+		/*Ninja ninja = new Ninja();
+		
+		ninja.setArma(new Chacos());
+		ninja.setMision("pasar nivel uno");*/
+		
+	ApplicationContext contenedor = new ClassPathXmlApplicationContext("/context.xml");
+	
+	Ninja ninja = (Ninja) contenedor.getBean("ninja");
+	
+	log.debug("arma del ninja: " + ninja.getArma());
+		
+	}
+	
+}
